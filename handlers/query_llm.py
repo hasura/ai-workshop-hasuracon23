d2a2a065089b22a95c6958dde6b09112b5ba63a1
Abhinav Gupta <abhinav@hasura.io>
2023-06-20 09:26:18 -0700

    Add call to LLM

--- handlers/query_llm.py
+++ handlers/query_llm.py
@@ -1,31 +1,63 @@
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 
+import os
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+
 # GraphQL endpoint URL
 GRAPHQL_ENDPOINT = "http://localhost:8080/v1/graphql"
 
 
-def query_llm(request, headers):
+def get_prompt(request):
     user_query = request['input']['user_query']
 
     # Add authenticated session variables as headers along with the admin secret
     gql_headers = request['session_variables']
-    gql_headers['x-hasura-admin-secret'] = headers['x-hasura-admin-secret']
+    gql_headers['x-hasura-admin-secret'] = 'secret'
 
     # Create a GraphQL client with the request transport
-    transport = RequestsHTTPTransport(url=GRAPHQL_ENDPOINT, headers=gql_headers)
+    transport = RequestsHTTPTransport(
+        url=GRAPHQL_ENDPOINT, headers=gql_headers)
     client = Client(transport=transport)
 
     # Send the GraphQL request
     gql_query = gql("""
-            query getChunks($user_query: text!) {
-                DocumentChunk(where: { vector: { near_text: $user_query}}) {
-                    chunk_text
+            query getItems($user_query: text!) {
+                Resume(where: { vector: { near_text: $user_query}}, limit: 5) {
+                    content
+                    application_id
                 }
             }
         """)
     result = client.execute(gql_query, variable_values={
                             'user_query': user_query})
 
-    # TODO: Call LLM
-    return str(result)
+    resumes = result['data']['Resume']
+
+    prompt = """
+    You are a helpful Question Answering bot. 
+    You are provided with contetn from a few resumes content and a question.
+    Answer the question based on the content of the resumes.
+    Provide your reasoning.
+
+    Question: """
+    prompt += user_query
+
+    for resume in resumes:
+        prompt += "Resume:"
+        prompt += resume["content"]
+        prompt += "with Application ID: "
+        prompt += resume["application_id"]
+        prompt += "\n"
+
+    return prompt
+
+def query_llm(request, headers):
+    llm = OpenAI(model="text-davinci-003",
+                 openai_api_key=os.environ['OPENAI_APIKEY'])
+    prompt = get_prompt(request)
+    chain = LLMChain(llm=llm, prompt=PromptTemplate.from_template(prompt))
+    return str(chain.run())
